479134091ce1fdb94c85cf960e818d3631d1c28a
Zachery Wagner <zacherywagner@gmail.com>
2024-04-02 15:49:54 -0700

    Added position to `Tile`

--- BarrelGameSwiftUI/Board/Tile.swift
+++ BarrelGameSwiftUI/Board/Tile.swift
@@ -7,23 +7,26 @@
 
 import Foundation
 
-/// Model
+/// Data model representing a `Tile` in the `Board`
 struct Tile {
     enum State: String {
         case empty
         case red
         case blue
     }
+
+    /// State represented by how the Tile is being occupied
+    public private(set) var state: State = .empty
+
+    /// The position of the Tile in relation to its matrix.  This should never change.
+    public let position: (row: Int, column: Int)
     
-    public var state: State = .empty
-    
-    private var position: (row: Int, column: Int) = (0, 0)
-    
-    public var stateDescrpition: String {
-        return state.rawValue
+    init(state: State, position: (row: Int, column: Int)) {
+        self.state = state
+        self.position = position
     }
     
-    init(state: State) {
+    public mutating func updateState(_ state: State) {
         self.state = state
     }
 }
