4b8f8c4d28a1b9d875aaa483eee32d8971fdcb0c
Zachery Wagner <zacherywagner@gmail.com>
2024-04-01 19:39:43 -0700

    Moved Tile

---
+++ BarrelGameSwiftUI/Board/Tile.swift
@@ -0,0 +1,29 @@
+//
+//  Tile.swift
+//  BarrelGameSwiftUI
+//
+//  Created by Zachery Wagner on 3/29/24.
+//
+
+import Foundation
+
+/// Model
+struct Tile {
+    enum State: String {
+        case empty
+        case red
+        case blue
+    }
+    
+    public var state: State = .empty
+    
+    private var position: (row: Int, column: Int) = (0, 0)
+    
+    public var stateDescrpition: String {
+        return state.rawValue
+    }
+    
+    init(state: State) {
+        self.state = state
+    }
+}
