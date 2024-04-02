3fa408464c376f719a51e968ce48970350c3a51d
Zachery Wagner <zacherywagner@gmail.com>
2024-04-02 15:50:05 -0700

    Refactored board to handle Tile positions

--- BarrelGameSwiftUI/Board/Board.swift
+++ BarrelGameSwiftUI/Board/Board.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Board {
+class Board {
     private var matrix = [[Tile]]()
     
     private var rowCount: Int
@@ -18,21 +18,26 @@ struct Board {
     
     // MARK: Initialization
     
-    init(rows: Int, columns: Int, startingState: Tile.State) {
+    init(rows: Int, columns: Int) {
         self.rowCount = rows
         self.columnCount = columns
-        self.matrix = createMatrix(rows: rows, columns: columns, startingState: startingState)
     }
 
     // MARK: Public
     
-    public func getTile(row: Int, column: Int) -> Tile? {
-        guard row < rowCount - 1,
-              column < columnCount - 1 else {
+    
+    /// Fetch the state of a tile at a given position
+    /// - Parameters:
+    ///   - row: The row of the tile
+    ///   - column: The column of the tile
+    /// - Returns: The state of the tile
+    public func getTileState(row: Int, column: Int) -> Tile.State? {
+        guard row < rowCount,
+              column < columnCount else {
             Logger.error(BoardError.failedToGetTile(row, column))
             return nil
         }
-        return matrix[row][column]
+        return matrix[row][column].state
     }
 
     /// Generates and assigns a randomized board to `matrix` that is ready to be played
@@ -41,98 +46,117 @@ struct Board {
     /// - Half of the filled tiles are red
     /// - Half of the filled tiles are blue
     /// - The arrangement of the filled tiles is random
-    public mutating func generateGameReadyBoard() {
+    public func generateGameReadyBoard() {
         Logger.info("generateGameReadyBoard")
-        // Create our starting arrays of each type
-        let startingAmount: Int = totalTiles / 3
-        let redArray = Array(repeating: Tile(state: .red), count: startingAmount)
-        let blueArray = Array(repeating: Tile(state: .blue), count: startingAmount)
-        let emptyArray = Array(repeating: Tile(state: .empty), count: startingAmount)
+
+        // Reset the matrix
+        matrix = createMatrix(rows: rowCount, columns: columnCount, startingState: .empty)
         
-        // Create a combined randomized array
-        var combinedArray = redArray + blueArray + emptyArray
-        combinedArray.shuffle()
+        // Get a list of randomized states
+        let tileStates = getRandomizedTileStates()
         
         // Filter the randomized array into the matrix
         for row in 0...rowCount - 1 {
             for column in 0...columnCount - 1 {
-                guard let tile = combinedArray.first else {
-                    print("[ERROR] \(BoardError.failedToGenerateGameBoard.localizedDescription)")
-                    return
-                }
-                matrix[row][column] = tile
-                combinedArray.removeFirst()
+                let index = row * columnCount + column // Calculate the flat index
+                let state = tileStates[index] // Directly access the state
+                matrix[row][column] = Tile(state: state, position: (row: row, column: column))
             }
         }
     }
 
-    public mutating func moveRight(row: Int) {
+
+    /// Creates and returns a randomized array of tile states based on `rowCount` and `columnCount`
+    /// - Returns: A shuffled array of `Tile.State`
+    private func getRandomizedTileStates() -> [Tile.State] {
+        Logger.info("getRandomizedTileStates")
+        
+        // Get the counts for each array.  This negates truncation issues.
+        let filledCount = totalTiles * 2 / 3
+        let redCount = filledCount / 2
+        let blueCount = filledCount - redCount
+        let emptyCount = totalTiles - filledCount
+
+        // Create starting arrays of each state
+        let startingAmount: Int = totalTiles / 3
+        let redArray = Array(repeating: Tile.State.red, count: redCount)
+        let blueArray = Array(repeating: Tile.State.blue, count: blueCount)
+        let emptyArray = Array(repeating: Tile.State.empty, count: emptyCount)
+        
+        // Create a combined randomized array of types
+        let combinedArray = redArray + blueArray + emptyArray
+        return combinedArray.shuffled()
+    }
+
+    public func moveRight(row: Int) {
         Logger.info("moveRight(row): \(row)")
-        let nonEmptyTiles: [Tile] = matrix[row].filter { $0.state != .empty }
-        let emptyTilesCount = matrix[row].count - nonEmptyTiles.count
+        guard row < rowCount else { return }
         
-        // Fill the beginning of the row with empty tiles
-        matrix[row] = Array(repeating: Tile(state: .empty), count: emptyTilesCount)
+        // Get the row's states
+        var statesInRow = matrix[row].map { $0.state }
         
-        // Append the non-empty tiles to the end of the row
-        matrix[row] += nonEmptyTiles
-    }
-    
-    public mutating func moveLeft(row: Int) {
-        Logger.info("moveLeft(row): \(row)")
-        let nonEmptyTiles: [Tile] = matrix[row].filter { $0.state != .empty }
-        let emptyTilesCount = matrix[row].count - nonEmptyTiles.count
+        // Get the non empty states
+        let nonEmptyStates = statesInRow.filter { $0 != .empty }
         
-        // Append the non-empty tiles to the end of the row
-        matrix[row] = nonEmptyTiles
+        // Cunt the empty states so we no how many to add to the left
+        let emptyStatesCount = statesInRow.count - nonEmptyStates.count
 
-        // Fill the beginning of the row with empty tiles
-        matrix[row] += Array(repeating: Tile(state: .empty), count: emptyTilesCount)
+        // Rearrange the row in the correct sequence of states
+        statesInRow = Array(repeating: .empty, count: emptyStatesCount) + nonEmptyStates
+
+        // Update the states of the tiles in the matrix based on our state sequence
+        for (column, state) in statesInRow.enumerated() {
+            matrix[row][column].updateState(state)
+        }
     }
     
-    public mutating func moveUp(column: Int) {
+    public func moveLeft(row: Int) {
+        Logger.info("moveLeft(row): \(row)")
+        guard row < rowCount else { return }
+
+        var statesInRow = matrix[row].map { $0.state}
+        let nonEmptyStates = statesInRow.filter({ $0 != .empty })
+        let emptyStateCount = statesInRow.count - nonEmptyStates.count
+        
+        // Create an array of the appropriately ordered states
+        statesInRow = nonEmptyStates + Array(repeating: .empty, count: emptyStateCount)
+        
+        for (column, state) in statesInRow.enumerated() {
+            matrix[row][column].updateState(state)
+        }
+    }
+    
+    public func moveUp(column: Int) {
         Logger.info("moveUp(column): \(column)")
-        var nonEmptyTiles: [Tile] = []
-        var emptyTilesCount = 0
-        
-        // Collect non-empty tiles and count empty tiles in the column
-        for row in matrix.indices {
-            if matrix[row][column].state != .empty {
-                nonEmptyTiles.append(matrix[row][column])
-            } else {
-                emptyTilesCount += 1
-            }
-        }
-        
-        // Reconstruct the column with non-empty tiles at the top
-        for row in 0..<nonEmptyTiles.count {
-            matrix[row][column] = nonEmptyTiles[row]
-        }
-        for row in nonEmptyTiles.count..<matrix.count {
-            matrix[row][column] = Tile(state: .empty)
+        guard column < columnCount else { return }
+
+        var statesInColumn = matrix.map { $0[column].state }
+        let nonEmptyStates = statesInColumn.filter { $0 != .empty }
+        let emptyStatesCount = statesInColumn.count - nonEmptyStates.count
+
+        // Create the new state sequence for the column
+        statesInColumn = nonEmptyStates + Array(repeating: .empty, count: emptyStatesCount)
+
+        // Update the states of the tiles in the matrix
+        for (row, state) in statesInColumn.enumerated() {
+            matrix[row][column].updateState(state)
         }
     }
     
-    public mutating func moveDown(column: Int) {
+    public func moveDown(column: Int) {
         Logger.info("moveDown(column): \(column)")
-        var nonEmptyTiles: [Tile] = []
-        var emptyTilesCount = 0
-        
-        // Collect non-empty tiles and count empty tiles in the column
-        for row in matrix.indices {
-            if matrix[row][column].state != .empty {
-                nonEmptyTiles.append(matrix[row][column])
-            } else {
-                emptyTilesCount += 1
-            }
-        }
-        
-        // Reconstruct the column with non-empty tiles at the bottom
-        for row in 0..<emptyTilesCount {
-            matrix[row][column] = Tile(state: .empty)
-        }
-        for row in emptyTilesCount..<matrix.count {
-            matrix[row][column] = nonEmptyTiles[row - emptyTilesCount]
+        guard column < columnCount else { return }
+
+        var statesInColumn = matrix.map { $0[column].state }
+        let nonEmptyStates = statesInColumn.filter { $0 != .empty }
+        let emptyStatesCount = statesInColumn.count - nonEmptyStates.count
+
+        // Create the new state sequence for the column
+        statesInColumn = Array(repeating: .empty, count: emptyStatesCount) + nonEmptyStates
+
+        // Update the states of the tiles in the matrix
+        for (row, state) in statesInColumn.enumerated() {
+            matrix[row][column].updateState(state)
         }
     }
 
@@ -141,7 +165,7 @@ struct Board {
     private func createMatrix(rows: Int, columns: Int, startingState: Tile.State) -> [[Tile]] {
         Logger.info("createMatrix(rows: \(rows), columns: \(columns), startingState: \(startingState.rawValue)")
         let matrix = Array(
-            repeating: Array(repeating: Tile(state: startingState), count: columns),
+            repeating: Array(repeating: Tile(state: startingState, position: (row: 0, column: 0)), count: columns),
             count: rows)
         return matrix
     }
@@ -149,33 +173,42 @@ struct Board {
 
 extension Board {
     public func prettyPrintMatrix() {
-        var maxLength = 0
+        var maxStateLength = 0
+        var maxPositionLength = 0
+
+        // Determine the maximum length needed for state and position representations
         for row in matrix {
             for item in row {
-                let itemLength = "\(item.stateDescrpition)".count
-                if itemLength > maxLength {
-                    maxLength = itemLength
+                let stateLength = "\(item.state)".count
+                if stateLength > maxStateLength {
+                    maxStateLength = stateLength
+                }
+                
+                let positionLength = "(\(item.position.row),\(item.position.column))".count
+                if positionLength > maxPositionLength {
+                    maxPositionLength = positionLength
                 }
             }
         }
+
+        let maxLength = max(maxStateLength, maxPositionLength)
         
         // Iterate over each row in the matrix to print it
         for row in matrix {
-            // Start the row with a vertical bar
-            var rowString = "|"
-            
+            var rowStateString = "|"
+            var rowPositionString = "|"
+
             for item in row {
-                let itemString = "\(item.stateDescrpition)"
-                
-                // Create a padded version of the item string so that it aligns correctly in its column.
-                let paddedItem = itemString.padding(toLength: maxLength, withPad: " ", startingAt: 0)
-                
-                // Append the padded item string to the row string, followed by a vertical bar.
-                rowString += " \(paddedItem) |"
+                let stateString = "\(item.state)".padding(toLength: maxLength, withPad: " ", startingAt: 0)
+                rowStateString += " \(stateString) |"
+
+                let positionString = "(\(item.position.row),\(item.position.column))".padding(toLength: maxLength, withPad: " ", startingAt: 0)
+                rowPositionString += " \(positionString) |"
             }
-            
-            // Print the complete row string
-            print(rowString)
+
+            // Print the state row followed by the position row for each tile
+            print(rowStateString)
+            print(rowPositionString)
         }
     }
 }
