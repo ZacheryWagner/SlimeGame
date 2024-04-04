0f6aec5b030928ab23bc7a91aa2ba1dbcdee9d94
Zachery Wagner <zacherywagner@gmail.com>
2024-04-04 15:22:02 -0700

    Moved/organized files

---
+++ BarrelGameSwiftUI/Config/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  BarrelGameSwiftUI
+//
+//  Created by Zachery Wagner on 4/2/24.
+//
+
+import Foundation
+
+class Constants {
+    
+    /// Number of slime rows
+    static let rows: Int = 5
+    
+    /// Number of slime clolumns
+    static let columns: Int = 5
+    
+    /// Number of real pixels per pixel art pixel
+    static let pixelWidth: CGFloat = 4
+    
+    /// Pixels around the playable area
+    static let playableAreaMargin: CGFloat = pixelWidth * 4
+    
+    /// Pixels between slimes
+    static let slimePadding: CGFloat = pixelWidth * 3
+}
